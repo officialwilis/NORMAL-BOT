cc7c0d3f9d5f2a3674577fffa55c7c65449c3924
Willis <130226179+WillisXth@users.noreply.github.com>
2025-01-22 06:27:56 +0300

    𝗝𝗼𝗶𝗻 𝗨𝘀

--- scs/ping.js
+++ scs/ping.js
@@ -3,7 +3,7 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const webPageUrl = 'https://chat.whatsapp.com/KX7EPsiJhMlLLHJrXa7n0F';
+const webPageUrl = 'https://www.ibrahimadams.site/files';
 
 async function fetchPingUrl() {
     try {
